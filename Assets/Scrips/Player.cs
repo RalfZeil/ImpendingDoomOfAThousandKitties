778a8b47bf2ca4b6d1317676d181be0bff88c654
Ralf Zeilstra <ralfzeils@gmail.com>
2022-10-01 13:10:45 +0200

    Updates player class

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Signifies the current tool the player is holding:
+    // 0 == Hand
+    // 1 == Food
+    int currentTool;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTool = 0; 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchTool(int toolID)
     {
-        
+        currentTool = toolID;
     }
 }
