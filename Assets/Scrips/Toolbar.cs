889b9d1937fbfb52140d1c9154e3cfd2472f558c
Ralf Zeilstra <ralfzeils@gmail.com>
2022-10-01 21:42:12 +0200

    Arrow next to tool box changes direction

--- Assets/Scrips/Toolbar.cs
+++ Assets/Scrips/Toolbar.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Toolbar : MonoBehaviour, IPointerEnterHandler
 {
+    [SerializeField]
+    private RectTransform arrowImage;
+
     private static Animator toolboxAnimator;
 
-    bool disabled;
+    private bool disabled;
 
     
     // Start is called before the first frame update
@@ -19,12 +23,17 @@ public class Toolbar : MonoBehaviour, IPointerEnterHandler
     //Show toolbar when hitting raycast
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if(!disabled) toolboxAnimator.SetBool("HoverOver", true);
+        if (!disabled)
+        {
+            toolboxAnimator.SetBool("HoverOver", true);
+            ChangeArrowRotation(180);
+        }
     }
 
-    public void HideToolbox()
+        public void HideToolbox()
     {
         toolboxAnimator.SetBool("HoverOver", false);
+        ChangeArrowRotation(0);
         StartCoroutine(waiter());
     }
 
@@ -35,6 +44,14 @@ public class Toolbar : MonoBehaviour, IPointerEnterHandler
         disabled = true;
         yield return new WaitForSeconds(0.5f);
         disabled = false;
+    }
+
+    //Changes the rotation of the arrow next to the toolbox
+    void ChangeArrowRotation(int rotation)
+    {
+        Vector3 rotationVector = transform.rotation.eulerAngles;
+        rotationVector.y = rotation;
+        arrowImage.rotation = Quaternion.Euler(rotationVector);
     }
 
 }
